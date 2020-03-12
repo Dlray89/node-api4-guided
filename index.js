80b94c09ff1e78f2553ab6d004e57f27405d88d0
Dlray89 <dlrayjr89@gmail.com>
2020-03-12 13:02:23 -0400

    added DB_ENV

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require("./api/server.js");
 
-const port = process.env.PORT || 5000; //asigning port to the server
+const port = process.env.DB_ENV; //asigning port to the server
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
