e85ae417ffe94efd47d4512cadb4626ba23b0c47
Dlray89 <dlrayjr89@gmail.com>
2020-03-12 13:47:42 -0400

    fixed errors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require("dotenv").config()
 const server = require("./api/server.js");
 
-const port = process.env.DB_ENV || "devolopment"; //asigning port to the server
+const port = process.env.DB_ENV || "development"; //asigning port to the server
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
